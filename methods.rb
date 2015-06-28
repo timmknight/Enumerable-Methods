1e6704296a984830ba699de2cfb26e3888939c71
Tim Knight <timmilesknight@gmail.com>
2015-06-28 19:13:15 +0100

    First commit

---
+++ methods.rb
@@ -0,0 +1,33 @@
+module Enumerable
+	def my_each
+	end
+
+	def my_each_with_index
+		
+	end
+
+	def my_select
+		
+	end
+
+	def my_all?
+		
+	end
+
+	def my_any?
+		
+	end
+
+	def my_count
+		
+	end
+
+	def my_map
+		
+	end
+
+	def my_inject
+		
+	end
+
+end
