139b0fca48243c9dbfc7d5bb6fe737c8a8952034
Tim Knight <timmilesknight@gmail.com>
2015-06-29 18:43:32 +0100

    my_each_with_index done

--- methods.rb
+++ methods.rb
@@ -10,8 +10,18 @@ print [1,2,3].my_each
 # => 1,2,3
 
 	def my_each_with_index
+		return self unless block_given?
+    for i in 0...self.length
+      yield(self[i], i)
+    end
 	end
-	
+
+["a", "b", "c"].each_with_index do |item, index| 
+	puts item
+	puts index
+end
+# => a,0,b,1,c,2
+
 	def my_select
 		
 	end
